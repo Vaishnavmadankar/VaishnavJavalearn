58a112e4b97ec0528b5cb8359cd985caee295167
madan <madan@LAPTOP-IAG65HB2>
2023-03-16 23:05:04 +0530

    Git code 16march

---
+++ Javaproject/src/Package1/Starprint5.java
@@ -0,0 +1,22 @@
+package Package1;
+
+public class Starprint5
+{
+    public static void main(String[] args)
+    {
+        
+        for (int i= 0; i<= 4; i++)
+        {
+            for (int j=1; j<=4-i; j++)
+            {
+                System.out.print(" ");
+            }
+            for (int k=0;k<=i;k++)
+            {
+                System.out.print("*");
+            } 
+                System.out.println("");
+        }
+ 
+    }
+}
---
+++ Javaproject/src/Package1/TablePrint.java
@@ -0,0 +1,26 @@
+package Package1;
+
+import java.util.Scanner;
+
+public class TablePrint 
+{
+   public void tableprint(int number) 
+   {
+	for (int i = 1; i <=10; i++) 
+	{	
+     System.out.println(number+"*"+i+"="+number*i);
+	}
+	   
+   }
+	
+	public static void main(String[] args) {
+		System.out.println("Please enter the values of table");
+		Scanner S=new Scanner(System.in);
+		int number=S.nextInt();   
+		
+		TablePrint Obj=new TablePrint();
+		System.out.println("Table for "+number);
+        Obj.tableprint(number);
+        
+	}
+}
