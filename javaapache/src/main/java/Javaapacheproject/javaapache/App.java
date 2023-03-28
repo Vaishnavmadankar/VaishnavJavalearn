5e5f0a62cbc86ee13c801538f8d28aa33607706f
madan <madan@LAPTOP-IAG65HB2>
2023-03-28 22:12:05 +0530

    GIT

---
+++ javaapache/src/main/java/Javaapacheproject/javaapache/App.java
@@ -0,0 +1,13 @@
+package Javaapacheproject.javaapache;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
